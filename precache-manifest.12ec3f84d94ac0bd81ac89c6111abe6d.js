9ddb35af65d0bb605aaec02486e4a4b9b5c9d36b
zmax101 <69588727+zmax101@users.noreply.github.com>
2020-10-01 01:57:24 +0300

    dep

---
+++ precache-manifest.12ec3f84d94ac0bd81ac89c6111abe6d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb51d27412d1fed4c133",
+    "url": "/VueJS-tasks-SPA/css/app.8ab72734.css"
+  },
+  {
+    "revision": "608a61e2b8e62018ca48",
+    "url": "/VueJS-tasks-SPA/css/chunk-02ac6958.55a5e7e4.css"
+  },
+  {
+    "revision": "a55a72b01b76d94e6c71",
+    "url": "/VueJS-tasks-SPA/css/chunk-6d54ed0d.4f03c4c1.css"
+  },
+  {
+    "revision": "bc99d9df492203dd89e3",
+    "url": "/VueJS-tasks-SPA/css/chunk-7629f83a.854b1016.css"
+  },
+  {
+    "revision": "b0f1f526a59305150742",
+    "url": "/VueJS-tasks-SPA/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "2218c2cd685f4810eb5625197d4608aa",
+    "url": "/VueJS-tasks-SPA/index.html"
+  },
+  {
+    "revision": "cb51d27412d1fed4c133",
+    "url": "/VueJS-tasks-SPA/js/app.101389d6.js"
+  },
+  {
+    "revision": "608a61e2b8e62018ca48",
+    "url": "/VueJS-tasks-SPA/js/chunk-02ac6958.ed613092.js"
+  },
+  {
+    "revision": "a55a72b01b76d94e6c71",
+    "url": "/VueJS-tasks-SPA/js/chunk-6d54ed0d.ac75b8e2.js"
+  },
+  {
+    "revision": "bc99d9df492203dd89e3",
+    "url": "/VueJS-tasks-SPA/js/chunk-7629f83a.af31d785.js"
+  },
+  {
+    "revision": "b0f1f526a59305150742",
+    "url": "/VueJS-tasks-SPA/js/chunk-vendors.359f19eb.js"
+  },
+  {
+    "revision": "bcece312d1a93cfd43972ed203239f0b",
+    "url": "/VueJS-tasks-SPA/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueJS-tasks-SPA/robots.txt"
+  }
+]);
